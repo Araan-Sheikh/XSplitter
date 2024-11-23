1f3fe47ef30a0a978d2667716955f692ca40f3d1
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2024-11-23 19:19:00 +0530

    Update user-management.tsx

--- components/user-management.tsx
+++ components/user-management.tsx
@@ -23,7 +23,7 @@ export function UserManagement({ members, onMemberAdded }: UserManagementProps) 
     e.preventDefault();
     
     const newMember: Member = {
-  id: Member,
+  id: "",
   name,
   email,
   preferredCurrency
